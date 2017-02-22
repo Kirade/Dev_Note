c0861e5af66882da778bdd88c093e94c6a37cd0d
허재영 <jay.jaeyoung@gmail.com>
2017-02-22 10:25:49 +0900

    Create Models for View functions
    * 게시판, 회원 모델 제작
    * 차후 외래키 관련 연결 필요 / 뷰 함수 연결 필요 / 데이터베이스 migrate 필요

--- tangerine/tangerine/web/models.py
+++ tangerine/tangerine/web/models.py
@@ -1,5 +1,60 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    name : 상품의 이름
+    price : 상품의 가격
+    inventory : 상품 재고 수
+    detail : 상품 설명 (optional)
+    """
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+    detail = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    """
+    name : 회원의 이름
+    login_id : 로그인 아이디
+    login_password : 로그인 비밀번호
+    address : 기본 배송 주소
+    contact : 연락처
+    email : 이메일
+    register_date : 회원가입일
+    """
+    name = models.CharField(max_length=100)
+    login_id = models.CharField(max_length=100)
+    login_password = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    email = models.EmailField()
+    register_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Board(models.Model):
+    """
+    title : 게시글 제목
+    writer : 게시글 작성자
+    date : 게시글 작성 날짜
+    content : 게시글 내용
+    board_password : 게시글 비밀번호
+    """
+    title = models.CharField(max_length=100)
+    # writer = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    content = models.TextField() # 나중에 UI 패드 적용가능 (생활코딩 참조)
+    board_password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- tangerine/tangerine/web/views.py
+++ tangerine/tangerine/web/views.py
@@ -1,8 +1,60 @@
+# coding=utf-8
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+    """
+    데이터를 나열하는 것을 포함하지 않는
+    일반적인 뷰를 보여주기 위해서 TemplateView 클래스를 사용한다.
+    """
+    template_name = 'web/index.html'
 
-def index(request):
-    return HttpResponse("Hello world!")
+"""
+
+1. URL 라우팅을 받아서 뷰 클래스로 온다.
+2. 해당하는 뷰 클래스에서 적절한 템플릿, 모델등을 오버라이딩을 한다.
+
+인덱스는 Product 모델에서 존재하는 상품들의 목록을 불러온뒤, 템플릿에 렌더링한다.
+
+상품을 클릭하면, 상품의 디테일이므로 DetailView클래스를 이용하여 상품 상세를 표시한다.
+
+상품을 주문할 때에는 주문을 관리하는 모델이 필요할 것 같다.
+- 주문 목록 저장, 해당 주문 번호에 대한 사용자 매칭?
+- 사용자 모델이 필요한가? 필요하다. : 마이페이지, 게시판 글쓰기 기능
+
+게시판, 회원 모델 제작 ( 스키마 구성 )
+
+
+
+"""
+
+
+# 밑에는 예시 클래스
+"""
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+
+        Return the last five published questions (not including those set to be
+        published in the future).
+
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+
+        Excludes any questions that aren't published yet.
+
+        return Question.objects.filter(pub_date__lte=timezone.now())
+"""
